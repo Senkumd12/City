b13bce86264f8d426becc44341955d113a70b0f6
XVioterX <fearfear325125@gmail.com>
2024-06-24 14:28:36 +0300

    Update اكس3 (1).js

--- plugins/اكس3 (1).js
+++ plugins/اكس3 (1).js
@@ -3,7 +3,7 @@ const handler = async (m, {conn, usedPrefix, command}) => {
   const room = Object.values(conn.game).find((room) => room.id.startsWith('tictactoe') && [room.game.playerX, room.game.playerO].includes(m.sender));
   if (room == undefined) return conn.sendButton(m.chat, '*[❗] انت لست في غرفه*', wm, null, [['لبدا مباراه جديده اكتب', `${usedPrefix}اكس مباراه جديده`]], m);
   delete conn.game[room.id];
-  await m.reply('*[ ✔ ] تم اذاله الغرفه*');
+  await m.reply('*[ ✔ ] تم ازالة الغرفة*');
 };
 handler.command = /^(delttt|deltt|delxo|اكس-حذف)$/i;
 handler.fail = null;
