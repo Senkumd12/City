20eb7296af2ef8ff722fd187ac52bf456f62a1fa
XVioterX <fearfear325125@gmail.com>
2024-06-24 15:33:58 +0300

    Update sticker-wm.js

--- plugins/sticker-wm.js
+++ plugins/sticker-wm.js
@@ -1,23 +1,24 @@
 import { addExif } from '../lib/sticker.js'
 let handler = async (m, { conn, text }) => {
-if (!m.quoted) throw '*[❗𝐈𝐍𝐅𝐎❗] 𝚁𝙴𝚂𝙿𝙾𝙽𝙳𝙴 𝙰𝙻 𝚂𝚃𝙸𝙲𝙺𝙴𝚁 𝚀𝚄𝙴 𝙳𝙴𝚂𝙴𝙰 𝙰𝙶𝚁𝙴𝙶𝙰𝚁 𝚄𝙽 𝙿𝙰𝚀𝚄𝙴𝚃𝙴 𝚈 𝚄𝙽 𝙽𝙾𝙼𝙱𝚁𝙴*'
+if (!m.quoted) throw '╰⊱❗️⊱ *خطأ* ⊱❗️⊱╮\nرد على المصلق تبي تخليه على حقوقك'
 let stiker = false
 try {
 let [packname, ...author] = text.split('|')
 author = (author || []).join('|')
 let mime = m.quoted.mimetype || ''
-if (!/webp/.test(mime)) throw '*[❗ملاحظه❗] يرجى الرد على الملصق الذي تريد سرقة حقوقه*'
+if (!/webp/.test(mime)) throw '╰⊱❗️⊱ *خطأ* ⊱❗️⊱╮\nرد على ملصق الي تبي تغير حقوقه وضيف اسم الي تبي'
 let img = await m.quoted.download()
-if (!img) throw '*[❗ملاحظه❗] يرجى الرد على الملصق الذي تريد سرقة حقوقه*'
-stiker = await addExif(img, packname || global.packname, author || global.author)
+if (!img) throw '╰⊱❗️⊱ *خطأ* ⊱❗️⊱╮\nرد على ملصق الي تبي تغير حقوقه وضيف اسم الي تبي'
+stiker = await addExif(img, packname || '', author || '')
 } catch (e) {
 console.error(e)
 if (Buffer.isBuffer(e)) stiker = e
 } finally {
-if (stiker) conn.sendFile(m.chat, stiker, 'wm.webp', '', m, false, { asSticker: true })
-else throw '*[❗𝐈𝐍𝐅𝐎❗] 𝙻𝙾 𝚂𝙸𝙴𝙽𝚃𝙾, 𝙰𝙻𝙶𝙾 𝙵𝙰𝙻𝙻𝙾.. 𝙲𝙾𝚁𝚁𝙾𝙱𝙾𝚁𝙴 𝚀𝚄𝙴 𝙷𝙰𝚈𝙰 𝚁𝙴𝚂𝙿𝙾𝙽𝙳𝙸𝙳𝙾 𝙰 𝚄𝙽 𝚂𝚃𝙸𝙲𝙺𝙴𝚁 𝚈 𝙷𝙰𝚈𝙰 𝙰𝙶𝚁𝙴𝙶𝙰𝙳𝙾 𝚄𝙽 𝙽𝙾𝙼𝙱𝚁𝙴 𝙳𝙴 𝙿𝙰𝚀𝚄𝙴𝚃𝙴 𝚈 𝚄𝙽 𝙽𝙾𝙼𝙱𝚁𝙴 𝙳𝙴 𝚄𝚂𝚄𝙰𝚁𝙸𝙾*'
+if (stiker) conn.sendFile(m.chat, stiker, 'sticker.webp', '',m, true, { contextInfo: { 'forwardingScore': 200, 'isForwarded': false, externalAdReply:{ showAdAttribution: false, title: wm, body: `⚡ 𝑽𝑬𝑵𝑶𝑴 𝑿𝑶𝑽 ⚡`, mediaType: 2, sourceUrl: nn, thumbnail: imagen1}}}, { quoted: m })
+else throw '╰⊱❗️⊱ *خطأ* ⊱❗️⊱╮\nيحدث شي ما خطأ يرجى محاولة مرة اخرى لاحقًا '
 }}
 handler.help = ['wm <packname>|<author>']
 handler.tags = ['sticker']
-handler.command = /^سرقة|اسرق|حقوق$/i
+handler.command = /^سرقة|سرقه|حقوق|حقوقي|wm$/i
+handler.limit = 1
 export default handler
