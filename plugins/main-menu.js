d5d466c74b7b495c42ff4e64fe978b94a5cef6f8
XVioterX <fearfear325125@gmail.com>
2024-06-28 11:41:43 +0300

    Update main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -70,7 +70,7 @@ let handler = async (m, { conn, usedPrefix, usedPrefix: _p, __dirname, text, isP
 `.trim()
 
     conn.sendMessage(m.chat, {
-    captain: str
+    caption: str,
     image: { url: imageUrl }, 
         mentions: [m.sender, global.conn.user.jid],
         gifPlayback: true, gifAttribution: 0
