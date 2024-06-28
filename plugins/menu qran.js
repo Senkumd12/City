eaee96b72dd9e0e87a1c06c4aec3ddcb6c1bed97
XVioterX <fearfear325125@gmail.com>
2024-06-28 12:34:16 +0300

    Update menu qran.js

--- plugins/menu qran.js
+++ plugins/menu qran.js
@@ -1,27 +1,30 @@
 let handler = async (m, { conn }) => {
   let who = m.quoted ? m.quoted.sender : (m.mentionedJid && m.mentionedJid[0]) ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender;
   let imageUrl = 'https://telegra.ph/file/33dfde90b872532a79488.png';
-  let { name } = global.db.data.users[who];
+  let name = conn.contacts[who] != undefined ? conn.contacts[who].vname || conn.contacts[who].notify : undefined;
+  
   m.react('☪');
+  
   let str = `
-                  ✥━─━⌬ 𝓧𝐂𝙖𝙨𝙥𝙚𝙧𝓧  ⌬━─━✥
-*【..≼قــســم الدين≽..】*
-                   ⋄━───═◞⬪⋇⬪◟═───━⋄
-*📖⤺┇〘اية_الكرسي〙*
-*📖⤺┇〘دين〙*
-                    ✥━─━⌬ 𝓧𝐂𝙖𝙨𝙥𝙚𝙧𝓧  ⌬━─━✥
-`;
+  ✥━─━⌬ 𝓧𝐂𝙖𝙨𝙥𝙚𝙧𝓧  ⌬━─━✥
+  *【..≼قــســم الدين≽..】*
+  ⋄━───═◞⬪⋇⬪◟═───━⋄
+  *📖⤺┇〘اية_الكرسي〙*
+  *📖⤺┇〘دين〙*
+  ✥━─━⌬ 𝓧𝐂𝙖𝙨𝙥𝙚𝙧𝓧  ⌬━─━✥
+  `;
+  
   conn.sendMessage(m.chat, {
     image: { url: imageUrl }, 
     caption: str,
-    mentions: [m.sender, conn.user.jid],
+    mentions: [{ tag: name, id: who }],
     gifPlayback: true,
     gifAttribution: 0
   }, { quoted: m });
 };
 
-handler.help = ['main']
-handler.tags = ['group']
-handler.command = ['1']
+handler.help = ['main'];
+handler.tags = ['group'];
+handler.command = ['1'];
 
 export default handler;
