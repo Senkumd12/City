5d5b3f2717cffb4031f75b9f3d2d0a058740a937
XVioterX <fearfear325125@gmail.com>
2024-06-28 05:35:22 +0300

    Update بوت.js

--- plugins/بوت.js
+++ plugins/بوت.js
@@ -3,16 +3,16 @@ let handler = async (m, { conn }) => {
   let name = conn.getName(who);
   let mentionedUsername = `@${who.replace(/@.+/, '')}`; // اسم المستخدم المنشن
 
-  let imgurl = 'https://telegra.ph/file/c761025b7d8387e2daa1d.jpg'; // رابط الصورة
+  let imgurl = 'https://telegra.ph/file/72f9dc162ba7c5be17bc0.jpg'; // رابط الصورة
   let filename = 'imgerror.jpg'; // اسم الملف
 
   // رسالة مع التعديل المطلوب
-  let msg = `*مــرحــبًــا ⌊ ${mentionedUsername} ⌉ انـا غـوكـو • تـحـتـاج الـى مـسـاعـدة؟ ، اكـتـب﹙.اوامــر﹚*\n\n> *مـمـنـوع اسـتـخـدام الـبـوت فـي الـدردشـات الـخـاصـة!*`;
+  let msg = `*ًمــرحــبــا ⌊ ${mentionedUsername} ⌉ انـا كـاسـبـر • تـحـتـاج الـى مـسـاعـدة؟ ، اكـتـب﹙.اوامــر﹚*\n\n> *مـمـنـوع اسـتـخـدام الـبـوت فـي الـدردشـات الـخـاصـة!*`;
 
   conn.sendFile(m.chat, imgurl, filename, msg, m, false, { thumbnail: Buffer.alloc(0), mentions: [who] });
 };
 
-handler.customPrefix = /^(بوت|bot|غوكو)$/i;
+handler.customPrefix = /^(بوت|bot|كاسبر)$/i;
 handler.command = new RegExp;
 
 export default handler;
