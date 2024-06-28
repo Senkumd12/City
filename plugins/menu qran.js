701e3a4b7e28e28f376eae0c53fd8401fadc12d9
XVioterX <fearfear325125@gmail.com>
2024-06-28 12:03:26 +0300

    Update menu qran.js

--- plugins/menu qran.js
+++ plugins/menu qran.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn }) => {
   let imageUrl = 'https://telegra.ph/file/33dfde90b872532a79488.png'
   let { name } = global.db.data.users[who]
   m.react('🕋')
-let str = `                  ✥━─━⌬ 𝓧𝐂𝙖𝙨𝙥𝙚𝙧𝓧  ⌬━─━✥
+  let str = `                  ✥━─━⌬ 𝓧𝐂𝙖𝙨𝙥𝙚𝙧𝓧  ⌬━─━✥
 *【..≼قــســم الدين≽..】*
                    ⋄━───═◞⬪⋇⬪◟═───━⋄
 *📖⤺┇〘اية_الكرسي〙*
@@ -13,10 +13,11 @@ let str = `                  ✥━─━⌬ 𝓧𝐂𝙖𝙨𝙥𝙚𝙧𝓧  �
   conn.sendMessage(m.chat, {
     image: { url: imageUrl }, 
     caption: str,
-     mentions: [m.sender,global.conn.user.jid],
-     gifPlayback: true,gifAttribution: 0
-       }, { quoted: m });
-   };
+    mentions: [who], // Assuming you want to mention the user whose data you're fetching
+    gifPlayback: true,
+    gifAttribution: 0
+  }, { quoted: m });
+};
 
 handler.help = ['main']
 handler.tags = ['group']
