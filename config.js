afd097f4da06bedddd73989be1780625427e1803
Senkumd12 <agaligue66@gmail.com>
2024-08-24 04:06:56 +0100

    Update config.js

--- config.js
+++ config.js
@@ -2,25 +2,25 @@ import { watchFile, unwatchFile } from 'fs'
 import chalk from 'chalk'
 import { fileURLToPath, pathToFileURL } from 'url'
 
-global.owner = [['201153263863', '🌩️ ẉa.me/𝓧𝐂𝙖𝙨𝙥𝙚𝙧𝓧‖.peị!‽ 👑', true], 
- ['201153263863', '🌩️ ẉa.me/𝓧𝐂𝙖𝙨𝙥𝙚𝙧𝓧‖.peị!‽ 👑', true],  ['201153263863'], ['201153263863'], ['201153263863'], ['201153263863'], ['201153263863'], ['201153263863'], ['201153263863'], ['201153263863'], ['201153263863']]
+global.owner = [['212658594530', '🌩️ ẉa.me/𝓧senku𝓧‖.peị!‽ 👑', true], 
+ ['201153263863', '🌩️ ẉa.me/𝓧𝐂𝙖𝙨𝙥𝙚𝙧𝓧‖.peị!‽ 👑', false],  ['201153263863'], ['201153263863'], ['201153263863'], ['201153263863'], ['201153263863'], ['201153263863'], ['201153263863'], ['201153263863'], ['201153263863']]
 
 //BETA: Si quiere evitar escribir el número que será bot en la consola, agregué desde aquí entonces:
 //Sólo aplica para opción 2 (ser bot con código de texto de 8 digitos)
-global.botNumberCode = '' //Ejemplo: +201153263863
+global.botNumberCode = '+212626817129' //Ejemplo: +201153263863
 global.confirmCode = ''
 
-global.animxscans = ['201153263863']
-global.suittag = ['201153263863']
+global.animxscans = ['212658594530']
+global.suittag = ['212658594530']
 global.mods = []
 global.prems = []
 
-global.packname = '『 201153263863 』'
-global.author = '『 كـاسـبـر بــــوت 』'
-global.wm = '『كـاسـبـر بــــوت 』'
-global.wm2 = '『 كـاسـبـر بــــوت 』'
-global.azami = '『كـاسـبـر بــــوت 』'
-global.cb = '『 كـاسـبـر بــــوت 』'
+global.packname = '『 حقوق عمك سينكَو 』'
+global.author = '『 سينكو بــــوت 』'
+global.wm = '『سينكو بــــوت 』'
+global.wm2 = '『 سينكو بــــوت 』'
+global.azami = '『سينكو بــــوت 』'
+global.cb = '『 سينكو بــــوت 』'
 
 global.vs = 'V2 • 1.0.5'
 global.library = 'Baileys'
@@ -28,8 +28,8 @@ global.baileys = '@whiskeysockets/baileys'
 global.lenguaje = 'Español'
 global.KMA = '╰━━━〔 *🛡️ 1.7.9* 〕━━━━━⬣'
 global.menudi = ['⛶','❏','⫹⫺']
-global.dev = '© 𝓧𝐂𝙖𝙨𝙥𝙚𝙧𝓧'
-global.devnum = '201153263863'
+global.dev = '© 𝓧senku𝓧'
+global.devnum = '21265854530'
 
 let file = fileURLToPath(import.meta.url)
 watchFile(file, () => { unwatchFile(file)
